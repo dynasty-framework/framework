7c0307fda633ccdb699f3977dbebd4176a9bd4e0
zhaotao@yuanh.net <zhaotao@yuanh.net>
2019-05-27 13:29:45 +0800

    删除不必要的依赖

--- portal-consumer/src/main/java/cn/dynasty/zt/portal/consumer/PortalConsumerApplication.java
+++ portal-consumer/src/main/java/cn/dynasty/zt/portal/consumer/PortalConsumerApplication.java
@@ -4,10 +4,8 @@ import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServl
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @ClassName: PortalConsumerApplication
@@ -34,10 +32,5 @@ public class PortalConsumerApplication {
         return registrationBean;
     }
 
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
 
 }
